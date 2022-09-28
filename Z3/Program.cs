c90e958e35130b871a2925622df8be65e47e024f
Vika <mandarinkavikabakalova@gmail.com>
2022-09-28 20:15:38 +0400

    Задача 3

---
+++ Z3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 3. Сформируйте двумерный массив из неповторяющихся случайных двузначных чисел
+// (размер массива не более 50 элементов). Напишите программу, которая будет 
+//построчно выводить массив.
+
+Console.WriteLine($"Введите размер массива ");
+int n =Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n,n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Random rnd= new Random();
+        int t= rnd.Next(10,100);
+        while (true)
+        {
+            if (! Contains (array, t))
+                break;
+            t= rnd.Next(10,100);    
+        }
+        array[i,j] = Convert.ToInt32( new Random().Next(10,50));
+    }
+}
+bool Contains (int[,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            if (array[i,j] == number)
+            return true;
+         }
+    }
+    return false;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    Console.Write(array[i,j]+ " " );
+    Console.WriteLine(); 
+}
+
